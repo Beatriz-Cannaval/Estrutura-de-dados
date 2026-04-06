5b7039736cb150b8eb1312dc793a14a6afaca3d3
Beatriz-Cannaval <cannavalbeatriz@gmail.com>
2026-04-06 14:35:48 -0300

    Update Ex-18.cpp

--- Ex-18.cpp
+++ Ex-18.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= 10; i++)
+        cout << n << " x " << i << " = " << n * i << endl;
+
     return 0;
 }
