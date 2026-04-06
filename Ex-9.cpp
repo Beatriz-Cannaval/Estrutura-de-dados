ddafacdb62bf286651ebe55f49cd2367b82c75a8
Beatriz-Cannaval <cannavalbeatriz@gmail.com>
2026-04-06 14:37:31 -0300

    Update Ex-9.cpp

--- Ex-9.cpp
+++ Ex-9.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    int numero;
+    float altura;
+
+    int numMaisAlto, numMaisBaixo;
+    float maiorAltura, menorAltura;
+
+    for (int i = 1; i <= 10; i++) {
+        cout << "Digite o numero do aluno: ";
+        cin >> numero;
+
+        cout << "Digite a altura (cm): ";
+        cin >> altura;
+
+        if (i == 1) {
+            maiorAltura = menorAltura = altura;
+            numMaisAlto = numMaisBaixo = numero;
+        } else {
+            if (altura > maiorAltura) {
+                maiorAltura = altura;
+                numMaisAlto = numero;
+            }
+
+            if (altura < menorAltura) {
+                menorAltura = altura;
+                numMaisBaixo = numero;
+            }
+        }
+    }
+
+    cout << "Aluno mais alto: " << numMaisAlto << " | Altura: " << maiorAltura << endl;
+    cout << "Aluno mais baixo: " << numMaisBaixo << " | Altura: " << menorAltura << endl;
+
     return 0;
 }
