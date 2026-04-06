2fe0aec13efa4b01a71da618c34a7ded0c3db484
Beatriz-Cannaval <cannavalbeatriz@gmail.com>
2026-04-06 14:35:27 -0300

    Update Ex-17.cpp

--- Ex-17.cpp
+++ Ex-17.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    int n;
+
+    do {
+        cin >> n;
+    } while (n < 1 || n > 10);
+
+    cout << "Valido";
+
     return 0;
 }
