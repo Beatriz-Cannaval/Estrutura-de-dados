a71902a0c9a8d4a32baa3dcdb561024e0ccfa8ce
Beatriz-Cannaval <cannavalbeatriz@gmail.com>
2026-04-06 14:36:58 -0300

    Update Ex-7.cpp

--- Ex-7.cpp
+++ Ex-7.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    for (int i = 1; i <= 500; i++) {
+        if (i % 5 == 0) {
+            cout << i << endl;
+        }
+    }
+
     return 0;
-}
+}}
