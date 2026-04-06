8fc3f8b594e3c1dde06cd56ad461599aca6e6021
Beatriz-Cannaval <cannavalbeatriz@gmail.com>
2026-04-06 14:36:06 -0300

    Update Ex-19.cpp

--- Ex-19.cpp
+++ Ex-19.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    float nota, soma = 0;
+    int total = 0;
+
+    cin >> nota;
+
+    while (nota >= 0) {
+        soma += nota;
+        total++;
+        cin >> nota;
+    }
+
+    if (total > 0)
+        cout << "Media: " << soma / total;
+    else
+        cout << "Sem dados";
+
     return 0;
 }
