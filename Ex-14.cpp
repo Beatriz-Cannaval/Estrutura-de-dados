829f2310a4b44b6c04533ca52b5040332f276177
Beatriz-Cannaval <cannavalbeatriz@gmail.com>
2026-04-06 14:34:37 -0300

    Update Ex-14.cpp

--- Ex-14.cpp
+++ Ex-14.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    int op;
+
+    do {
+        cout << "\n1 - Opcao 1\n2 - Opcao 2\n3 - Sair\n";
+        cin >> op;
+
+        switch(op) {
+            case 1: cout << "Escolheu 1"; break;
+            case 2: cout << "Escolheu 2"; break;
+            case 3: cout << "Saindo..."; break;
+            default: cout << "Invalido";
+        }
+
+    } while (op != 3);
+
     return 0;
 }
